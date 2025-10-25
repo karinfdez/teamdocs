17503aa7b3efae8f5ff279c885a223955b9b6c3a
karinfdez <karinfdez@yahoo.com>
2025-10-24 20:17:12 -0700

    Built the simulated fetching api ad get api requetss

--- src/features/files/api.ts
+++ src/features/files/api.ts
@@ -1,1 +1,24 @@
 // Mocked API
+
+interface FileItem {
+    id: string;
+    name: string;
+}
+
+const mockFiles: FileItem[] = []  //Simulates saved in DB
+
+export async function getFiles() {
+    await new Promise(res => setTimeout(res, 200)) // to simulate network delay
+    return mockFiles
+}
+
+export async function uploadFile(file: File): Promise<FileItem> {
+    await new Promise(res => setTimeout(res, 300)) // To simulate uploading time
+
+    const newFile = {
+        id: crypto.randomUUID(),
+        name: file.name,
+    };
+    mockFiles.push(newFile)
+    return newFile
+}
